a8e91b3dfaf10e0dfc89547da90bd263455ae5a0
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-02 09:22:28 +0200

    Setter with optional properties

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({name: 'myname'; age:20})
+const user = new User({ name: 'myname', age: 20 });
 
-console.log(user.get('name'))
-console.log(user.get('age'))
+user.set({ age: 999 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
 export class User {
@@ -8,6 +8,10 @@ export class User {
 
   get(propName: string): string | number {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
   }
 }
 
