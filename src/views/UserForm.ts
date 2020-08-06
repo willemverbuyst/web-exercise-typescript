c48e739946c104e34abc71ac348f1a6c02c76dd2
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-06 10:09:18 +0200

    Add eventsMap

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -2,11 +2,23 @@ export class UserForm {
   // reference to any html element
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      // 'hover:h1': this.onHoverHeader
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('hi there');
+  }
+
   template(): string {
     return `
     <div>
       <h1>User Form</h1>
       <input />
+      <button>Click me</button>
     </div>`;
   }
 
