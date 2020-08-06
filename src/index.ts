ca182ead46e6ff511e40d91810bed965c1b1f3fa
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-06 06:34:57 +0200

    Bind this with arrow function

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'sjaak', age: 80808 });
 
-// user.fetch();
-
-// setTimeout(() => {
-//   console.log(user);
-// }, 4000);
-
-// user.set({ age: 100 });
-
-// user.events.on('Change', () => {
-//   console.log('change!');
-// });
-
-// user.events.trigger('Change');
+console.log(user.get('name'));
 
 user.on('change', () => {
   console.log('user was changed');
 });
+
+user.trigger('change');
