ef5287f1dec6943bfb59a9119f4f84b592a43da6
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-06 15:04:13 +0200

    Add model properties

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,11 +1,13 @@
+import { User } from '../models/User';
+
 export class UserForm {
   // reference to any html element
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
-      // 'hover:h1': this.onHoverHeader
+      'mouseenter:h1': this.onHoverHeader,
     };
   }
 
@@ -13,18 +15,38 @@ export class UserForm {
     console.log('hi there');
   }
 
+  onHoverHeader(): void {
+    console.log('yo h1');
+  }
+
   template(): string {
     return `
     <div>
       <h1>User Form</h1>
+      <div>User name: ${this.model.get('name')}</div>
+      <div>User age: ${this.model.get('age')}</div>
       <input />
       <button>Click me</button>
     </div>`;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
